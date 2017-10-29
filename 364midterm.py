15e514e68d2575ba9af81543aa1c32bd80162065
bdpaul99 <bddpaul@me.com>
2017-10-29 16:21:33 -0400

    finished the project

--- 364midterm.py
+++ 364midterm.py
@@ -1,10 +1,22 @@
-from flask import Flask, request, render_template, redirect, url_for, flash
+from flask import Flask, request, render_template, redirect, url_for, flash, abort, make_response
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField
 from wtforms.validators import Required, Email
-
+import tweepy
 import requests
 import json
+
+
+
+consumer_key = 'CHgwrlRgRs9EhEAgmkINwhHnO'
+consumer_secret = 'IJmlsD2zY6Qo18NHh7yCLHVpJBT8n79e2WHxt0pdUojfdIyr1G'
+access_token = '336251929-k0N4ZmLuqnGKacqYiDwF74MuRk663qhIW2LXeVpT'
+access_token_secret = 'yhueP8ZVRytRvU7HIrH7WNocjaixS6vtWuSuBIkJDhCxe'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'somerelativelysecurepassword'
@@ -15,9 +27,12 @@ class twitterForm(FlaskForm):
 	username = StringField("Enter a username to search for on twitter", validators = [Required()])
 	submit = SubmitField('Submit')
 
+
+
 @app.route('/')
 def home():
-    return "Hello, world!"
+    return redirect("http://localhost:5000/index")
+    
 
 @app.route('/index')
 def index():
@@ -27,22 +42,45 @@ def index():
 
 
 
-@app.route('/result', methods = ['GET', 'POST'])
+@app.route('/twitterresult', methods = ['GET', 'POST'])
 def result():
     form = twitterForm(request.form)
     if request.method == 'POST':
         username = form.username.data
+        resp = make_response(render_template('twitterresult.html', name = username))
+        resp.set_cookie('username', username)
         
-        return render_template('twitterresult.html',name = username)
+        return resp
     
+@app.route('/<user_name>/tweets')
+def twitter_users(user_name):
+    try:
+        timeline = api.user_timeline(id = user_name)
+        
+    except:
+        abort(500)
+    
+    
+    return render_template('user_tweets.html', result = timeline, name = user_name)
+
+@app.route('/<user_name>/followers')
+def twitter_followers(user_name):
+    try:
+        followers = api.followers(id = user_name)
+    except:
+        abort(500)
+    return render_template('twitter_followers.html', result = followers, name = user_name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
 
 
 
-
-
-
-
-
+@app.errorhandler(500)
+def internal_server(e):
+    return render_template('500.html')
 
 
 
